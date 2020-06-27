5e47681b3e4a78ad106436bf92f945c1e31e25f5
ducanh <ngducanh1223@gmail.com>
2020-06-27 17:04:20 +0700

    Restyle login register

---
+++ resources/views/user/user_login.blade.php
@@ -0,0 +1,151 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<link rel="stylesheet" type="text/css" href="{{asset('css/loginForm_css/user_login.css')}}">
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+<script src="{{asset('js/user_login.js')}}"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+            <div class="panel panel-login">
+                <div class="panel-heading">
+                    <div class="row" >
+                        <div class="col-xs-4" style="margin-left: -3px;">
+                            <a style="font-size: 70px" href="#" class="active" id="login-form-link">Login</a>
+                        </div>
+                        <div class="col-xs-4" style="margin-left: -42px;">
+                            <span style="font-size: 70px">/</span>
+                        </div>
+                        <div class="col-xs-4" style="margin-left: -71px;">
+                            <a style="font-size: 70px" href="#" id="register-form-link">Register</a>
+                        </div>
+                    </div>
+                    <hr>
+                </div>
+                <div class="panel-body">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            @if($errors->any())
+                                <div class="alert alert-danger">
+                                    @foreach($errors->all() as $err)
+                                        <li>{{$err}}</li>
+                                    @endforeach
+                                </div>
+                            @endif
+                            @if(session('message'))
+                                <div class="alert alert-success">
+                                    {{session('message')}}
+                                </div>
+                            @endif
+                            <form id="login-form" action="/user/login" method="post" role="form" style="display: block;">
+                                @csrf()
+                                <div class="form-group">
+                                    <input type="text" name="username" id="username" tabindex="1" class="form-control" placeholder="Username" value="">
+                                </div>
+                                <div class="form-group">
+                                    <input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Password">
+                                </div>
+                                <div class="form-group text-center">
+                                    <input type="checkbox" tabindex="3" class="" name="remember" id="remember">
+                                    <label for="remember"> Remember Me</label>
+                                </div>
+                                <div class="form-group">
+                                    <div class="row">
+                                        <div class="col-sm-6 col-sm-offset-3">
+                                            <input type="submit"
+                                                   style="background-color: #EC7A5C;width: 223%;margin-left: -151px;"
+                                                   name="login-submit"
+                                                   id="login-submit"
+                                                   tabindex="4"
+                                                   class="form-control btn btn-login"
+                                                   value="Log In">
+                                        </div>
+                                    </div>
+                                </div>
+                                {{--<div class="form-group">
+                                    <div class="row">
+                                        <div class="col-lg-12">
+                                            <div class="text-center">
+                                                <a href="http://phpoll.com/recover" tabindex="5" class="forgot-password">Forgot Password?</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>--}}
+                            </form>
+                            <form id="register-form" action="/user/register-process" method="post" role="form" style="display: none;">
+                                @csrf()
+                                <div class="form-group">
+                                    <input type="text" name="username" id="username" tabindex="1" class="form-control" placeholder="Username" value="">
+                                </div>
+                                <div class="form-group">
+                                    <input type="email" name="email" id="email" tabindex="1" class="form-control" placeholder="Email Address" value="">
+                                </div>
+                                <div class="form-group">
+                                    <input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Password">
+                                </div>
+                                <div class="form-group">
+                                    <input type="password" name="confirm-password" id="confirm-password" tabindex="2" class="form-control" placeholder="Confirm Password">
+                                </div>
+                                <div class="form-group">
+                                    <input type="text" name="full_name" id="inputFullname" tabindex="1" class="form-control" placeholder="Full Name" value="">
+                                </div>
+                                <div class="form-group">
+                                    <input type="text" name="address" id="inputAddress" tabindex="1" class="form-control" placeholder="Addresss" value="">
+                                </div>
+                                <div class="form-group">
+                                    <div class="input-group">
+                                        <input class="form-control" id="date" name="DOB" placeholder="Date of birth" type="text" readonly/>
+                                    </div>
+                                </div>
+                                <div class="form-group" style="text-align: left">
+                                    <label>Gender</label>
+                                    <div class="form-check">
+                                        <label class="form-check-label">
+                                            <input type="radio" class="form-check-input" name="gender" id="1" value="option1" checked=""> Female
+                                        </label>
+                                    </div>
+                                    <div class="form-check">
+                                        <label class="form-check-label">
+                                            <input type="radio" class="form-check-input" name="gender" id="2" value="option2"> Male
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <input type="text" name="phone" id="inputPhone" tabindex="1" class="form-control" placeholder="Phone Number" value="">
+                                </div>
+                                <div class="form-group">
+                                    <div class="row">
+                                        <div class="col-sm-6 col-sm-offset-3">
+                                            <input type="submit" name="register-submit" id="register-submit" tabindex="4" class="form-control btn btn-register" value="Register Now">
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Include Date Range Picker -->
+<script type="text/javascript"
+        src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
+<link rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
+<script>
+    $(document).ready(function () {
+        var date_input = $('input[name="DOB"]'); //our date input has the name "date"
+        var container = $('.bootstrap-iso form').length > 0 ? $('.bootstrap-iso form').parent() : "body";
+        date_input.datepicker({
+            format: 'dd/mm/yyyy',
+            container: container,
+            todayHighlight: true,
+            autoclose: true,
+        })
+    })
+</script>
+
